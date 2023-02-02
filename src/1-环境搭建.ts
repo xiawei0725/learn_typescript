f06194bb3efa7de3e871aae058a805e1cb26cac8
xiawei <54879490@qq.com>
2023-02-03 00:27:23 +0800

    1-环境搭建

---
+++ src/1-环境搭建.ts
@@ -0,0 +1,26 @@
+/**
+    
+全局安装typescript
+npm i -g typescript 
+
+tsc --init 生成tsconfig.json
+*/
+
+//  {
+//   "compilerOptions": {
+//     "target": "ES5"
+//     "module": "ES6",
+//     "outDir": "./dist"
+//     },
+//     "include": ["src/**/*"]
+//  }
+
+/**
+ * 运行 tsc -w 即可监听src下ts 文件 生成对应的js文件
+ * 接下来就可以愉快的学习ts了
+ */
+
+
+let a = 100
+const ab = 10
+export { }
